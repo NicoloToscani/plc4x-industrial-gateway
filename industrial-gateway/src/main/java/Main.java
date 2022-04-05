c608f28c0cfa28cb1f0d32b73417a4aa8ac8a705
Nicolò Toscani <toscani.nicolo90@gmail.com>
2022-04-05 09:20:09 -0700

    Add files via upload

--- industrial-gateway/src/main/java/Main.java
+++ industrial-gateway/src/main/java/Main.java
@@ -1,20 +1,32 @@
-import java.io.Console;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-
+import java.util.concurrent.TimeoutException;
 import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.PlcConnection;
 import org.apache.plc4x.java.api.messages.PlcReadRequest;
 import org.apache.plc4x.java.api.messages.PlcReadResponse;
+import org.apache.plc4x.java.scraper.config.triggeredscraper.JobConfigurationTriggeredImplBuilder;
+import org.apache.plc4x.java.scraper.config.triggeredscraper.ScraperConfigurationTriggeredImpl;
+import org.apache.plc4x.java.scraper.config.triggeredscraper.ScraperConfigurationTriggeredImplBuilder;
+import org.apache.plc4x.java.scraper.exception.ScraperException;
+import org.apache.plc4x.java.scraper.triggeredscraper.TriggeredScraperImpl;
+import org.apache.plc4x.java.scraper.triggeredscraper.triggerhandler.collector.TriggerCollector;
+import org.apache.plc4x.java.scraper.triggeredscraper.triggerhandler.collector.TriggerCollectorImpl;
+import org.apache.plc4x.java.utils.connectionpool.PooledPlcDriverManager;
 
 public class Main {
 
+	static Connection connection = null;
 	public static void main(String[] args) {
+		
+		
+		// Scraper: query scheduler
+		ScraperConfigurationTriggeredImplBuilder schedluer = new ScraperConfigurationTriggeredImplBuilder();
+		
 		
 		// JDBC driver name and database URL
 	    String driver = "org.apache.iotdb.jdbc.IoTDBDriver";
@@ -24,7 +36,7 @@ public class Main {
 	    String username = "root";
 	    String password = "root";
 
-	    Connection connection = null;
+	    
 	    try {
 	      Class.forName(driver);
 	      connection = DriverManager.getConnection(url, username, password);
@@ -33,10 +45,19 @@ public class Main {
 	    } catch (SQLException e) {
 	      e.printStackTrace();
 	    }
-	   
+	    
 	    
 		// S7 connection string
 		String connectionString = "s7://192.168.100.1";
+		
+		schedluer.addSource("s7Connection", connectionString);
+		
+		JobConfigurationTriggeredImplBuilder jobBuilder = schedluer.job("s7job", "(SCHEDULED,10000)");
+		
+		jobBuilder.source("s7Connection");
+		jobBuilder.field("frequency", "%DB1.DB4.0:REAL");
+		jobBuilder.build();
+		ScraperConfigurationTriggeredImpl scraperConfig = schedluer.build();
 		
 		
 		try {
@@ -73,10 +94,9 @@ public class Main {
 			System.out.println("start_cmd: " + response.getObject("start_cmd"));
 			System.out.println("stop_cmd: " + response.getObject("stop_cmd"));
 			System.out.println("frequency: " + response.getObject("frequency"));
+				
 			
 			Object value = (Float)response.getObject("frequency");
-			
-			
 			
 			// Insert into IoTDB
 		    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO ? (TIMESTAMP, ?) VALUES (?, ?)")) {
@@ -88,8 +108,47 @@ public class Main {
 	        } catch (SQLException e) {
 	            System.out.println(e.toString());
 	        }
-			
-			
+		    
+		    
+		    try {
+	            PlcDriverManager plcDriverManager = new PooledPlcDriverManager();
+	            TriggerCollector triggerCollector = new TriggerCollectorImpl(plcDriverManager);
+	            TriggeredScraperImpl scraper = new TriggeredScraperImpl(scraperConfig, (jobName, sourceName, results) -> {
+	            	
+	            	try {
+	            		
+						PlcReadResponse responsePolling = readRequest.execute().get(5000, TimeUnit.MILLISECONDS);
+						System.out.println("frequency polling: " + responsePolling.getObject("frequency"));
+						
+						// Insert into IoTDB - ToDo
+						
+						Object valueP = (Float)responsePolling.getObject("frequency");
+						
+						// Insert into IoTDB
+					    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO ? (TIMESTAMP, ?) VALUES (?, ?)")) {
+				            statement.setString(1, "root.energy.pac2200");
+				            statement.setString(2, "frequency");
+				            statement.setLong(3, System.currentTimeMillis());
+				            statement.setFloat(4,(float) valueP);
+				            statement.execute();
+				        } catch (SQLException e) {
+				            System.out.println(e.toString());
+				        }
+						
+					} catch (InterruptedException | ExecutionException | TimeoutException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	           
+	            }, triggerCollector);
+	            scraper.start();
+	            triggerCollector.start();
+	        } catch (ScraperException e) {
+	            System.out.println("Error starting the scraper");
+	        }
+		    
+		   
+		    
 			
 		}catch (Exception e) {
 			
@@ -98,8 +157,14 @@ public class Main {
 		
 		
 		
+		
+		
+		
 
 
 	}
+	
+	
+	
 
 }
